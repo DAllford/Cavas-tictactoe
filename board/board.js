ef29e04a18cfedc97e8cd64118c909d8046b1975
Daniel Allford <dan.allford@blackcatsolutions.co.uk>
2016-08-13 22:24:55 +0100

    tidying up a bit

--- board/board.js
+++ board/board.js
@@ -1,6 +1,53 @@
 angular.module("tictactoe", [])
 
     .directive('ticTacToe', function() {
+
+
+        function drawGridLines(ctx) {
+            drawVerticalLine(ctx, 100);
+            drawVerticalLine(ctx, 200);
+            drawHorizontalLine(ctx, 100);
+            drawHorizontalLine(ctx, 200);
+            function drawVerticalLine (ctx, xcoord) {
+                ctx.moveTo(xcoord, 0);
+                ctx.lineTo(xcoord, 300);
+                ctx.stroke();
+            }
+            function drawHorizontalLine (ctx, ycoord) {
+                ctx.moveTo(0, ycoord);
+                ctx.lineTo(300, ycoord);
+                ctx.stroke();
+            }
+        };
+
+        function drawCleanBoard(ctx) {
+            ctx.beginPath();
+            ctx.clearRect(0, 0, 300, 300);
+            drawGridLines(ctx);
+        }
+
+        function drawCounter(xcoord, yCoord, color, context) {
+            context.beginPath();
+            context.arc(xcoord, yCoord, 40, 0, 2 * Math.PI, false);
+            context.fillStyle = color;
+            context.fill();
+            context.strokeStyle = 5;
+            context.stroke();
+        };
+
+        function drawBoard(board, context) {
+            drawCleanBoard(context);
+            for(var x=0; x<=2; x++) {
+                for(var y=0; y<=2; y++) {
+                    if(board[x][y].color){
+                        var xcoord = x * 100 + 50;
+                        var yCoord = y * 100 + 50;
+                        drawCounter(xcoord, yCoord, board[x][y].color, context);
+                    }
+                }
+            }
+        }
+
         return {
             templateUrl: "/tictactoe/board/board.html",
             link: function(scope, element, attrs) {
@@ -11,26 +58,23 @@ angular.module("tictactoe", [])
             },
             controller: function ($scope, boardState) {
                 $scope.addCounter = function(counter) {
-                    var xcoord = counter.x * 100 + 50;
-                    var yCoord = counter.y * 100 + 50;
-                    if(boardState.addCounter(counter)){
-                        drawCounter(xcoord, yCoord, counter.color, $scope.ctx);
-                    }
+                    boardState.addCounter(counter);
+                    drawBoard(boardState.getBoard(), $scope.ctx);
                 };
 
                 $scope.placeCounter = function (event) {
-                    var xcoord = Math.floor(event.clientX/100);
-                    var ycoord = Math.floor(event.clientY/100);
+                    var xcoord = Math.min(Math.floor(event.clientX/100), 2);
+                    var ycoord = Math.min(Math.floor(event.clientY/100), 2);
                     $scope.addCounter({x: xcoord, y: ycoord, color: boardState.getPlayerColor()});
                 };
 
-                $scope.$on("reset", function() {
+                $scope.$on("reset-board", function() {
                     drawCleanBoard($scope.ctx);
                     boardState.resetBoard();
                 });
 
                 $scope.backgroundColor = function () {
-                    return boardState.getWinner();
+                    return boardState.getWinner() || "white";
                 }
             }
         };
@@ -38,115 +82,6 @@ angular.module("tictactoe", [])
 
     .controller("clearCtrl", function($scope, $rootScope) {
         $scope.resetBoard = function() {
-            $rootScope.$broadcast("reset");
-        }
-    })
-    .service('boardState', function () {
-        var playerColors = ["blue", "red"];
-        var playerNum = 0;
-        function emptyBoard() {
-            return [[], [], []];
-        }
-        var counters = emptyBoard();
-        var switchPlayer = function () {
-            playerNum = (playerNum+1)%2;
-        };
-        return {
-            getPlayerColor: function() {
-                return playerColors[playerNum];
-            },
-            addCounter: function(newCounter) {
-                if(!counters[newCounter.x][newCounter.y]) {
-                    counters[newCounter.x][newCounter.y] = {color: newCounter.color};
-                    switchPlayer();
-                    return true;
-                }
-                return false;
-            },
-            resetBoard: function() {
-                counters = emptyBoard();
-            },
-            getWinner: function() {
-                return getWinner(counters) || "white";
-            }
+            $rootScope.$broadcast("reset-board");
         }
     });
-
-function getWinner(board) {
-    function checkLine(line) {
-        var mappedLine = line.map(function(square) {
-            square = square || {color: null};
-            return square.color;
-        });
-        if(mappedLine[0] && (mappedLine[0] == mappedLine[1] && mappedLine[1] == mappedLine[2])) {
-            return mappedLine[0];
-        } else {
-            return null;
-        }
-    }
-
-    function getLines(board) {
-        function getRows(board) {
-            return angular.copy(board);
-        }
-        function getCols(board) {
-            var cols = [];
-            for(var i = 0; i<=2; i++) {
-                cols[i] = [];
-                for(var j=0; j<=2; j++) {
-                    cols[i][j] = board[j][i];
-                }
-            }
-            return cols;
-        }
-        function getDiag(board) {
-            return [
-                [board[0][0], board[1][1], board[2][2]],
-                [board[2][0], board[1][1], board[0][2]
-                ]
-            ]
-        }
-
-        return getRows(board).concat(getCols(board)).concat(getDiag(board));
-    }
-
-    var lines = getLines(board);
-    var winner = null;
-    lines.forEach(function(line) {
-        winner = winner || checkLine(line);
-    });
-    return winner;
-}
-
-
-function drawGridLines(ctx) {
-    drawVerticalLine(ctx, 100);
-    drawVerticalLine(ctx, 200);
-    drawHorizontalLine(ctx, 100);
-    drawHorizontalLine(ctx, 200);
-    function drawVerticalLine (ctx, xcoord) {
-        ctx.moveTo(xcoord, 0);
-        ctx.lineTo(xcoord, 300);
-        ctx.stroke();
-    }
-    function drawHorizontalLine (ctx, ycoord) {
-        ctx.moveTo(0, ycoord);
-        ctx.lineTo(300, ycoord);
-        ctx.stroke();
-    }
-};
-
-function drawCleanBoard(ctx) {
-    ctx.beginPath();
-    ctx.clearRect(0, 0, 300, 300);
-    drawGridLines(ctx);
-}
-
-function drawCounter(xcoord, yCoord, color, context) {
-    context.beginPath();
-    context.arc(xcoord, yCoord, 40, 0, 2 * Math.PI, false);
-    context.fillStyle = color;
-    context.fill();
-    context.strokeStyle = 5;
-    context.stroke();
-};
--- board/boardService.js
+++ board/boardService.js
@@ -1,2 +1,85 @@
 angular.module("tictactoe")
 
+    .service('boardState', function () {
+        var playerColors = ["blue", "red"];
+        var playerNum = 0;
+        function emptyBoard() {
+            var board = [[], [], []];
+            return board.map(function() {
+                return [{color: null}, {color: null}, {color: null}];
+            });
+        }
+        function canAddCounter(newCounter) {
+            return !counters[newCounter.x][newCounter.y].color && (getWinner(counters) == null) ;
+        }
+        var counters = emptyBoard();
+        var switchPlayer = function () {
+            playerNum = (playerNum+1)%2;
+        };
+
+        return {
+            getPlayerColor: function() {
+                return playerColors[playerNum];
+            },
+            addCounter: function(newCounter) {
+                if(canAddCounter(newCounter)) {
+                    counters[newCounter.x][newCounter.y] = {color: newCounter.color};
+                    switchPlayer();
+                }
+            },
+            resetBoard: function() {
+                counters = emptyBoard();
+            },
+            getWinner: function() {
+                return getWinner(counters);
+            },
+            getBoard: function() {
+                return angular.copy(counters);
+            }
+        }
+    });
+
+function getWinner(board) {
+    function getLineWinner(line) {
+        var squareColors = line.map(function(square) {
+            return square.color;
+        });
+        if(squareColors[0] && (squareColors[0] == squareColors[1] && squareColors[1] == squareColors[2])) {
+            return squareColors[0];
+        } else {
+            return null;
+        }
+    }
+
+    function getLines(board) {
+        function getRows(board) {
+            return angular.copy(board);
+        }
+        function getCols(board) {
+            var cols = [];
+            for(var i = 0; i<=2; i++) {
+                cols[i] = [];
+                for(var j=0; j<=2; j++) {
+                    cols[i][j] = board[j][i];
+                }
+            }
+            return cols;
+        }
+        function getDiag(board) {
+            return [
+                [board[0][0], board[1][1], board[2][2]],
+                [board[2][0], board[1][1], board[0][2]
+                ]
+            ]
+        }
+
+        return getRows(board).concat(getCols(board)).concat(getDiag(board));
+    }
+
+    var lines = getLines(board);
+    var winner = null;
+    lines.forEach(function(line) {
+        winner = winner || getLineWinner(line);
+    });
+    return winner;
+};
